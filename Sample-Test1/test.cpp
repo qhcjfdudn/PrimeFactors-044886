ca7aac03bf8d5d2cc095b74b28ad38d04a5397c4
hack.jang <hack.jang@samsung.com>
2024-07-03 11:25:59 +0900

    feature 54

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -30,3 +30,7 @@ TEST_F(PrimeFactorFixture, Of6) {
 	expected = { 2, 3 };
 	EXPECT_EQ(expected, prime_factor.of(6));
 }
+TEST_F(PrimeFactorFixture, Of9) {
+	expected = { 3, 3 };
+	EXPECT_EQ(expected, prime_factor.of(9));
+}
