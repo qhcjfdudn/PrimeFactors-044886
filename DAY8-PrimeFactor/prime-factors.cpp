79d039a5269606b3a47818828fb30b9a3bd53e2d
hack.jang <hack.jang@samsung.com>
2024-07-03 11:08:16 +0900

    feature 37p

--- DAY8-PrimeFactor/prime-factors.cpp
+++ DAY8-PrimeFactor/prime-factors.cpp
@@ -5,6 +5,9 @@ class PrimeFactor {
 public:
 	vector<int> of(int n) {
 		vector<int> result = {};
+		if (n == 2)
+			result.push_back(2);
+
 		return result;
 	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -8,5 +8,9 @@ TEST(PrimeFactors, Of1) {
 	PrimeFactor prime_factor;
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
-
+}
+TEST(PrimeFactors, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = { 2 };
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
