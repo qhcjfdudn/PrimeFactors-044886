36b0b2b291380a5d367be0e4c985d1287508457a
hack.jang <hack.jang@samsung.com>
2024-07-03 11:03:34 +0900

    feature 33p

---
+++ DAY8-PrimeFactor/prime-factors.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+using namespace std;
+
+class PrimeFactor {
+public:
+	vector<int> of(int n) {
+		return {};
+	}
+};
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,12 @@
 #include "pch.h"
 #include "../DAY8-PrimeFactor/prime-factors.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include <vector>
+using namespace std;
+
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
+
 }
