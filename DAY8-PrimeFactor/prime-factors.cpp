e1c2c74c284c762e73f7ac093ad5e23761e2f49d
hack.jang <hack.jang@samsung.com>
2024-07-03 11:23:47 +0900

    refactor 52

--- DAY8-PrimeFactor/prime-factors.cpp
+++ DAY8-PrimeFactor/prime-factors.cpp
@@ -5,20 +5,14 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-		if (number > 1)
-			if (number == 4) {
-				while (number % 2 == 0) {
-					result.push_back(2);
-					number /= 2;
+		if (number > 1) {
+			for (int divisor = 2; number > 1; ++divisor) {
+				while (number % divisor == 0) {
+					result.push_back(divisor);
+					number /= divisor;
 				}
 			}
-			else if (number == 6) {
-				result.push_back(2);
-				result.push_back(3);
-			}
-			else {
-				result.push_back(number);
-			}
+		}
 
 		return result;
 	}
